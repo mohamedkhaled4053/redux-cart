b7c187e67ce76f4263a58fb2a7a544de162a02e5
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-10-14 10:19:58 +0200

    add modal static code

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,9 +1,11 @@
 import CartContainer from './components/CartContainer';
+import Modal from './components/modal';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Modal />
       <Navbar />
       <CartContainer />
     </>
